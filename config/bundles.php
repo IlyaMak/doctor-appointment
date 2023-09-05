e7489fbe9aa735ad29bde2c344fe7c9960c6a13c
Ilya <ilmobdev@gmail.com>
2023-09-05 17:33:09 +0200

    Add user registration

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
---
+++ src/Controller/AppointmentController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppointmentController extends AbstractController {
+    #[Route('/appointment', name: 'appointment_list')]
+    public function appointmentList(): Response {
+        return $this->render('/appointment/appointment-list.html.twig');
+    }
+}
