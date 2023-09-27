f15fea95925a8b8a8998c91e2f4b3ae60bdc09dd
Ilya <ilmobdev@gmail.com>
2023-09-27 21:38:23 +0200

    #15. Add StripeTransaction entity

---
+++ src/Entity/StripeTransaction.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StripeTransactionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StripeTransactionRepository::class)]
+class StripeTransaction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $objectId = null;
+
+    #[ORM\Column]
+    private ?int $amount = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $currency = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getObjectId(): ?int
+    {
+        return $this->objectId;
+    }
+
+    public function setObjectId(int $objectId): static
+    {
+        $this->objectId = $objectId;
+
+        return $this;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getCurrency(): ?string
+    {
+        return $this->currency;
+    }
+
+    public function setCurrency(string $currency): static
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+}
