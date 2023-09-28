f9fd3a7b886f715dd4bf6d9e98850faa80aa4594
Ilya <ilmobdev@gmail.com>
2023-09-28 22:52:06 +0200

    #15. Add stripeCreatedAt and serverCreatedAt in StripeTransaction entity

--- src/Entity/StripeTransaction.php
+++ src/Entity/StripeTransaction.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\StripeTransactionRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: StripeTransactionRepository::class)]
@@ -14,20 +15,39 @@ class StripeTransaction
     private ?int $id = null;
 
     #[ORM\Column]
-    private ?string $objectId = null;
+    private string $objectId;
 
     #[ORM\Column]
-    private ?int $amount = null;
+    private int $amount;
 
     #[ORM\Column(length: 255)]
-    private ?string $currency = null;
+    private string $currency;
+
+    #[ORM\Column(options: ['default' => 'CURRENT_TIMESTAMP'])]
+    private DateTimeImmutable $stripeCreatedAt;
+
+    #[ORM\Column(options: ['default' => 'CURRENT_TIMESTAMP'])]
+    private DateTimeImmutable $serverCreatedAt;
+
+    public function __construct(
+        string $objectId,
+        int $amount,
+        string $currency,
+        DateTimeImmutable $stripeCreatedAt,
+    ) {
+        $this->objectId = $objectId;
+        $this->amount = $amount;
+        $this->currency = $currency;
+        $this->stripeCreatedAt = $stripeCreatedAt;
+        $this->serverCreatedAt = new DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getObjectId(): ?string
+    public function getObjectId(): string
     {
         return $this->objectId;
     }
@@ -39,7 +59,7 @@ class StripeTransaction
         return $this;
     }
 
-    public function getAmount(): ?int
+    public function getAmount(): int
     {
         return $this->amount;
     }
@@ -51,7 +71,7 @@ class StripeTransaction
         return $this;
     }
 
-    public function getCurrency(): ?string
+    public function getCurrency(): string
     {
         return $this->currency;
     }
@@ -59,6 +79,30 @@ class StripeTransaction
     public function setCurrency(string $currency): static
     {
         $this->currency = $currency;
+
+        return $this;
+    }
+
+    public function getStripeCreatedAt(): DateTimeImmutable
+    {
+        return $this->stripeCreatedAt;
+    }
+
+    public function setStripeCreatedAt(DateTimeImmutable $stripeCreatedAt): static
+    {
+        $this->stripeCreatedAt = $stripeCreatedAt;
+
+        return $this;
+    }
+
+    public function getServerCreatedAt(): DateTimeImmutable
+    {
+        return $this->serverCreatedAt;
+    }
+
+    public function setServerCreatedAt(DateTimeImmutable $serverCreatedAt): static
+    {
+        $this->serverCreatedAt = $serverCreatedAt;
 
         return $this;
     }
