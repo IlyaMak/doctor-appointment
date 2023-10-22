67fb277ae8563f35b14e303b58355a386bb3a1e8
Ilya <ilmobdev@gmail.com>
2023-10-22 19:09:15 +0200

    #49. Add language selector component

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,8 @@
 import './styles/app.scss';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import './styles/components-style.css'
+import './styles/language-flag.css';
 import addBackgroundSlot from './components/add-background-slot';
 
 addBackgroundSlot();
--- assets/security.js
+++ assets/security.js
@@ -1,2 +1,4 @@
 import './styles/auth.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import './styles/language-flag.css';
