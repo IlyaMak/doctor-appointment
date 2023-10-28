2e0bef4f3e11a9b5f017d3a48555da9d869d43b4
Ilya <ilmobdev@gmail.com>
2023-10-28 21:00:22 +0200

    #61. Split register action RegistrationController to 2 actions for a patient and a doctor

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,21 +2,18 @@
 
 namespace App\Controller;
 
-use App\Entity\Specialty;
 use App\Entity\User;
 use App\Form\DoctorRegistrationFormType;
 use App\Form\PatientRegistrationFormType;
-use App\Form\RegistrationTypeFormType;
 use App\Repository\UserRepository;
 use App\Security\EmailVerifier;
+use App\Service\RegistrationService as ServiceRegistrationService;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mime\Address;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
@@ -29,18 +26,16 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 #[Route('/{_locale<%app.supported_locales%>}')]
 class RegistrationController extends AbstractController
 {
-    private EmailVerifier $emailVerifier;
-
     public function __construct(
-        EmailVerifier $emailVerifier,
         #[Autowire(param: 'is_required_email_verification')]
         private bool $isEmailVerificationRequired,
         private TranslatorInterface $translator,
+        private ServiceRegistrationService $registrationService,
+        private EmailVerifier $emailVerifier
     ) {
-        $this->emailVerifier = $emailVerifier;
     }
 
-    #[Route('/register', name: 'app_register')]
+    #[Route('/patient-register', name: 'app_patient_register')]
     public function register(
         Request $request,
         UserPasswordHasherInterface $userPasswordHasher,
@@ -49,84 +44,95 @@ class RegistrationController extends AbstractController
         #[Autowire(service: 'security.authenticator.form_login.main')]
         FormLoginAuthenticator $authenticator,
         #[Autowire(env: 'EMAIL_ADDRESS')]
-        string $emailAddress,
+        string $emailAddress
     ): Response {
-
-        $registrationTypeForm = $this->createForm(RegistrationTypeFormType::class);
-        $registrationTypeForm->handleRequest($request);
-        /** @var int */
-        $type = $registrationTypeForm->get('type')->getData();
-        $isDoctor = 0 === $type;
         $user = new User();
-        $form = $this->createForm(
-            $isDoctor
-                ? DoctorRegistrationFormType::class
-                : PatientRegistrationFormType::class,
-            $user,
-        );
+        $form = $this->createForm(PatientRegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            /** @var string */
-            $name = $form->get('name')->getData();
-            /** @var string */
-            $plainPassword = $form->get('plainPassword')->getData();
-            /** @var string|Address */
-            $email = $user->getEmail();
-            $user->setName($name);
-            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
-            $user->setLanguage($request->getLocale());
+            $this->registrationService->setBasicUserProperties(
+                $form,
+                $user,
+                $userPasswordHasher,
+                $request
+            );
 
-            if ($isDoctor) {
-                /** @var ?Specialty */
-                $specialty = $form->get('specialty')->getData();
-                /** @var UploadedFile */
-                $avatarData = $form->get('avatar')->getData();
-                $user->setSpecialty($specialty);
-                $currentDateTime = new DateTime();
-                $currentDate = $currentDateTime->format('YmdHisv');
-                $localAvatarPath = $currentDate . ' . ' . $avatarData->getClientOriginalExtension();
-                $avatarData->move('resources', $localAvatarPath);
-                $user->setAvatarPath($localAvatarPath);
-                $user->setRoles([User::ROLE_DOCTOR]);
-            } else {
-                $user->setRoles([User::ROLE_PATIENT]);
-            }
+            $user->setRoles([User::ROLE_PATIENT]);
 
             $entityManager->persist($user);
             $entityManager->flush();
 
             if ($this->isEmailVerificationRequired) {
-                $this->emailVerifier->sendEmailConfirmation(
-                    'app_verify_email',
+                $this->registrationService->sendEmailConfirmation(
+                    $this->emailVerifier,
                     $user,
-                    (new TemplatedEmail())
-                        ->from(new Address($emailAddress, $this->translator->trans('doctor_appointment_bot_name')))
-                        ->to($email)
-                        ->subject($this->translator->trans('email_confirmation_subject'))
-                        ->htmlTemplate('security/confirmation_email.html.twig')
-                        ->context([
-                            'emailHeader' => $this->translator->trans('email_header'),
-                            'emailDescription' => $this->translator->trans('confirmation_email_description'),
-                            'emailLink' => $this->translator->trans('confirm_email_link'),
-                            'expireLinkDescription' => $this->translator->trans('expire_link_description'),
-                        ])
+                    $emailAddress,
+                    $this->translator
                 );
-
                 return $this->render('security/register_success.html.twig');
             } else {
                 $authenticatorManager->authenticateUser($user, $authenticator, $request);
-                if ($isDoctor) {
-                    return $this->redirectToRoute('schedule');
-                } else {
-                    return $this->redirectToRoute('patient_book_an_appointment');
-                }
+                return $this->redirectToRoute('patient_book_an_appointment');
             }
         }
 
         return $this->render('security/register.html.twig', [
             'registrationForm' => $form->createView(),
-            'registrationTypeForm' => $registrationTypeForm->createView(),
+        ]);
+    }
+
+    #[Route('/doctor-register', name: 'app_doctor_register')]
+    public function doctorRegister(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        EntityManagerInterface $entityManager,
+        UserAuthenticatorInterface $authenticatorManager,
+        #[Autowire(service: 'security.authenticator.form_login.main')]
+        FormLoginAuthenticator $authenticator,
+        #[Autowire(env: 'EMAIL_ADDRESS')]
+        string $emailAddress
+    ): Response {
+        $user = new User();
+        $form = $this->createForm(DoctorRegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->registrationService->setBasicUserProperties(
+                $form,
+                $user,
+                $userPasswordHasher,
+                $request
+            );
+
+            /** @var UploadedFile */
+            $avatarData = $form->get('avatar')->getData();
+            $currentDateTime = new DateTime();
+            $currentDate = $currentDateTime->format('YmdHisv');
+            $localAvatarPath = $currentDate . ' . ' . $avatarData->getClientOriginalExtension();
+            $avatarData->move('resources', $localAvatarPath);
+            $user->setAvatarPath($localAvatarPath);
+            $user->setRoles([User::ROLE_DOCTOR]);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            if ($this->isEmailVerificationRequired) {
+                $this->registrationService->sendEmailConfirmation(
+                    $this->emailVerifier,
+                    $user,
+                    $emailAddress,
+                    $this->translator
+                );
+                return $this->render('security/register_success.html.twig');
+            } else {
+                $authenticatorManager->authenticateUser($user, $authenticator, $request);
+                return $this->redirectToRoute('schedule');
+            }
+        }
+
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $form->createView()
         ]);
     }
 
@@ -138,7 +144,7 @@ class RegistrationController extends AbstractController
         if (null === $id) {
             $logger->error('User id is not exists');
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('app_patient_register');
         }
 
         $user = $userRepository->find($id);
@@ -146,7 +152,7 @@ class RegistrationController extends AbstractController
         if (null === $user) {
             $logger->error('User is not exists');
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('app_patient_register');
         }
 
         try {
@@ -154,7 +160,7 @@ class RegistrationController extends AbstractController
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $exception->getReason());
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('app_patient_register');
         }
 
         $this->addFlash('success', $this->translator->trans('verified_message_mark'));
