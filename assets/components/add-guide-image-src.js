54f194222c4cde0791d9fddb50577717652199f7
Ilya <ilmobdev@gmail.com>
2023-11-03 13:24:18 +0100

    #63. Fix class name. Fix images width

--- assets/components/add-guide-image-src.js
+++ assets/components/add-guide-image-src.js
@@ -1,7 +1,8 @@
 export default function () {
     function addGuideImageSrc(e) {
-        const increasedImage = document.querySelector('.increasedImage');
-        increasedImage.src = e.target.src;
+        const zoomedInImage = document.querySelector('.zoomedInImage');
+        console.log(e.target.src);
+        zoomedInImage.src = e.target.src;
     }
 
     const guideImages = document.querySelectorAll('.guideImage');
