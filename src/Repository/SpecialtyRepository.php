c8243e5fb76ca07fa4099962c8353313c9af958d
Ilya <ilmobdev@gmail.com>
2023-11-06 11:43:53 +0100

    #73. Add isApproved condition to get approved doctors

--- src/Repository/SpecialtyRepository.php
+++ src/Repository/SpecialtyRepository.php
@@ -61,6 +61,7 @@ class SpecialtyRepository extends ServiceEntityRepository
         $specialties = $queryBuilder
             ->leftJoin('sp.doctors', 'u')
             ->leftJoin(ScheduleSlot::class, 's', Join::WITH, 's.doctor = u')
+            ->andWhere('u.isApproved = true')
             ->andWhere('s.start > :currentDate')
             ->andWhere('s.patient IS NULL')
             ->setParameter('currentDate', $currentDate)
