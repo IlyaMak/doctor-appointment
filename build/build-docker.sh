e1c7c2ba179bb7222f092a6f48787627a68de8bb
Ilya <ilmobdev@gmail.com>
2023-11-11 17:29:01 +0100

    #79. Add build scripts

---
+++ build/build-docker.sh
@@ -0,0 +1,10 @@
+if [ -f '.env' ]
+then
+    docker compose build
+    docker compose up -d
+    docker compose exec php composer install
+    docker compose exec php npm i
+    docker compose exec php npm run build
+else
+    echo 'You need to create .env file in the root directory. Run: cp .env.example .env'
+fi
