2b5f0081e8e4a45e296e40d948c7ed89172d5beb
Ilya <ilmobdev@gmail.com>
2023-11-20 15:07:00 +0100

    Add verification_exception_message translation key

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -7,19 +7,23 @@ use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class UserChecker implements UserCheckerInterface
 {
     public function __construct(
         #[Autowire(param: 'is_required_email_verification')]
         private bool $isEmailVerificationRequired,
+        private TranslatorInterface $translator,
     ) {
     }
 
     public function checkPreAuth(UserInterface $user)
     {
         if ($user instanceof User && !$user->isVerified() && $this->isEmailVerificationRequired) {
-            throw new CustomUserMessageAuthenticationException('Your email is not verified!');
+            throw new CustomUserMessageAuthenticationException(
+                $this->translator->trans('verification_exception_message')
+            );
         }
     }
 
